8048a45147ce3930344de37f68e77fb8339ccb42
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-06 06:14:06 +0500

    Responsive Fixes

--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -14,10 +14,10 @@
 
     <div class="container mx-auto mb-40 p-4 mt-10">
         <div class="grid grid-cols-12 gap-4 justify-center">
-            <div class="col-span-3">
+            <div class="col-span-12 lg:col-span-3">
                 @include('auth.components.sidebar')
             </div>
-            <div class="col-span-9">
+            <div class="col-span-12 lg:col-span-9">
                 @if (Request::get('route') === 'submissions')
                     @include('auth.partials.submissions.manage', compact('submissions'))
                 @endif
